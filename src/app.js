90fde97ff489edbd3bb6857090b1fa960eae2d5d
HRLopezP <hrlp843@gmail.com>
2025-07-18 22:52:26 +0000

    resultado_final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-import "bootstrap";
-import "./style.css";
+let quien = ["Un duende", "El vecino", "Mi hermano", "Un fantasma", "El presidente"];
+let verbo = ["destruyó", "escondió", "hizo desaparecer", "incineró", "saboteó"];
+let que = ["las llaves", "el informe", "el documento", "la presentación", "el dinero"];
+let cuando = ["ayer en la noche.", "el fin de semana pasado.", "justo antes de salir.", "hace un momento.", "en la madrugada."];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function excusa(quien, verbo, que, cuando) {
+  
+  const aleatorio= Math.floor(Math.random() * 5);
+  
+  let quien_salida = quien[aleatorio];
+  let verbo_salida= verbo[aleatorio];
+  let que_salida = que[aleatorio];
+  let cuando_salida = cuando[aleatorio];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const oracion = quien_salida  + " " + verbo_salida + " " + que_salida + " " + cuando_salida;
+
+  return oracion;
+}
+
+const excusa_completa = excusa(quien, verbo, que, cuando);
+console.log(excusa_completa);
+
+document.getElementById("excusa").innerHTML = 
+  excusa_completa;
